87876a72095e4e60d5e79a9b32d3ef724d087018
prodev205 <godmiracle205@gmail.com>
2023-07-21 21:42:32 +0300

    mobile-menu done

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -2,6 +2,7 @@
   const mobileMenu = document.querySelector('.js-menu-container');
   const openMenuBtn = document.querySelector('.js-open-menu');
   const closeMenuBtn = document.querySelector('.js-close-menu');
+  const contactsMenu = document.querySelector('.js-contacts-menu');
 
   const toggleMenu = () => {
     const isMenuOpen =
@@ -15,8 +16,17 @@
     bodyScrollLock[scrollLockMethod](document.body);
   };
 
+  const toContacts = () => {
+    mobileMenu.classList.toggle('is-open');
+    const isMenuOpen =
+      openMenuBtn.getAttribute('aria-expanded') === 'false' || true;
+    openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
+    bodyScrollLock.enableBodyScroll(document.body);
+  };
+
   openMenuBtn.addEventListener('click', toggleMenu);
   closeMenuBtn.addEventListener('click', toggleMenu);
+  contactsMenu.addEventListener('click', toContacts);
 
   // Close the mobile menu on wider screens if the device orientation changes
   window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
